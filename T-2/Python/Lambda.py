47a332df7c7882017af17ee8dab5c2e4f8c8bc11
EkalSorathiya <125211462+EkalSorathiya@users.noreply.github.com>
2023-12-20 08:56:47 +0530

    Done

---
+++ T-2/Python/Lambda.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+s=lambda x:"Even" if x%2==0 else "Odd"
+print(s(10))
+print(s(11))
+a=lambda x,y:x+y
+print(a(10.5,10))
+
+
+# In[15]:
+
+
+l1=[1,2,3,4,5,6]
+l2=[1,2,3,4,5]
+sq=lambda x:x*x
+s=list(map(sq,l1))
+s1=set(map(lambda x,y:x**x+y**y,l1,l2))
+s1=tuple(map(lambda x,y:x**x+y**y,l1,l2))
+s1=list(map(lambda x,y:x**x+y**y,l1,l2))
+print(s)
+print(s1)
+
+
+# In[20]:
+
+
+l1=[1,2,3,4,5,6]
+from functools import *
+r=reduce(lambda x,y:x+y,l1)
+print(r)
+
+
+# In[24]:
+
+
+l=[1,2,3,4,5,6,7,8,9,10]
+f=set(filter(lambda x:x %2==0,l))
+f=tuple(filter(lambda x:x %2==0,l))
+f=list(filter(lambda x:x %2==0,l))
+print(f)
+
+
+# In[29]:
+
+
+# using lambda fun to get list whose elemenet are divisible 13 and 19
+l=[13,18,123,51,247]
+s=list(filter(lambda x:x%13==0 and x%19==0,l))
+print(s)
+
+
+# In[32]:
+
+
+# to segrigate two diff list with positive and nevative number
+
+l=[1,2,-3,0,4,-5,6,7,-8,9]
+p=list(filter(lambda c:c<0,l))
+print(p)
+n=list(filter(lambda c:c>=0,l))
+print(n)
+
+
+# In[68]:
+
+
+# for encrypt the string with rail fence cipher technique in rail fence cipher you can break string by any int number like 2,3,4
+# also decrypt the same key value 
+
+
+    
+
+
+# In[77]:
+
+
+s="This Message Is For Ekal"
+k=3
+m=""
+
+for i in range(key):
+    m+=s[i::3]
+print(m)
+
+l=len(s)
+p=l//k
+e=l%k
+
+p1=m[:(p+1)*e]
+p2=m[(p+1)*e:]
+msg=""
+
+for i in range(p+1):
+    if i<p:
+        msg+=p1[i::p+1]+p2[i::p]
+    else:
+        msg+=p1[i::p+1]    
+print(msg)    
+
+
+# In[84]:
+
+
+# wap to create a dict from a string in which key is a first letter of word and value is list of words in that start with the key
+
+s="wap to create a dict from a string in which key is a first letter of word and value is list of words in that start with the key"
+
+l=s.split()
+d={}
+for i in l:
+    fc=i[0].lower()
+    if fc in d:
+        d[fc].append(i)
+    else:
+        d[fc]=[i]
+    
+print(d)
+
+
+# In[6]:
+
+
+# all passible combination of 3 digit
+l=[1,2,2]
+for i in range(3):
+    for j in range(3):
+        for k in range(3):
+            if i!=j and j!=k and k!=i:
+                print(str(l[i])+str(l[j])+str(l[k]))
+            elif k==j:
+                print(str())
+
+
+# In[ ]:
+
+
+# a company has decided to give gifts the employee for that company has given the rank to each emp based on the ranlk company has mads certai rules to distribue the gift each emp must recive atlest 1 gigt emp having higher ranking get > number of gigt then their neighbours what is the min no. of gift req by the company
+emp=int(input("Enter No. of Employee"))
+rl=[]
+gl=[]
+for i in range(emp):
+    er=int(input("Enter Rank"))
+    rl.append(er)
+count=len(rl)
+for i in range(count-1):
+    if rl[i]>rl[i+1]:
+        gl.append(1)
+    elif rl[i]<rl[i+1]:
+        gl.append(2)
+print(rl)
+print(gl)
+
+
+# In[ ]:
+
+
+
+
