c121483cd86d40fb26878cf3b539953f7ec28abf
EkalSorathiya <125211462+EkalSorathiya@users.noreply.github.com>
2024-02-01 06:04:36 +0530

    Done

---
+++ T-4/FSD/JS File/Facto.js
@@ -0,0 +1,4 @@
+function fact(a){
+    var f=1+a/1+(a**2)/2+(a**3)/6+(a**4)/24;
+    document.write("Answer is =",f);
+}
---
+++ T-4/FSD/JS File/mul.js
@@ -0,0 +1,3 @@
+function mul(a,b,c){
+    document.write("Answer is =",a*b*c)
+}
